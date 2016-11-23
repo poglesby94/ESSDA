9486da22bd43fd74c38b6421ef414b32be61b768
poglesby94 <paul.oglesby94@icloud.com>
2016-11-22 23:09:44 -0500

    Home page implemented and ordering of clubs in admin page by alphabetical order

--- app/controllers/admin/clubs/new.js
+++ app/controllers/admin/clubs/new.js
@@ -4,13 +4,11 @@ export default Ember.Controller.extend({
      name:'',
      website:'',
      logo:'',
-     keyword:'',
 
      nameValid: Ember.computed.notEmpty('name'),
      websiteValid: Ember.computed.notEmpty('website'),
      logoValid: Ember.computed.notEmpty('logo'),
-     keywordValid: Ember.computed.notEmpty('keyword'),
-     isValid: Ember.computed.and('nameValid','websiteValid','logoValid','keywordValid'),
+     isValid: Ember.computed.and('nameValid','websiteValid','logoValid'),
      isDisabled: Ember.computed.not('isValid'),
 
      actions:{
@@ -18,15 +16,13 @@ export default Ember.Controller.extend({
               const clubName = this.get('name');
               const clubWebsite = this.get('website');
               const clubLogo = this.get('logo');
-              const clubKeyword = this.get('keyword');
               const newClub = this.store.createRecord('clubs',{
-                   name:clubName, website:clubWebsite, logo:clubLogo, keyword:clubKeyword
+                   name:clubName, website:clubWebsite, logo:clubLogo
               });
               newClub.save().then(()=>{
                    this.set('name','');
                    this.set('website','');
                    this.set('logo','');
-                   this.set('keyword','');
                    this.transitionToRoute('admin.clubs.index');
               });
           },
@@ -34,7 +30,6 @@ export default Ember.Controller.extend({
                this.set('name','');
                this.set('website','');
                this.set('logo','');
-               this.set('keyword','');
                this.transitionToRoute('admin.clubs.index');
           }
      }
--- app/models/clubs.js
+++ app/models/clubs.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   website: DS.attr('string'),
-  logo: DS.attr('string'),
-  keyword: DS.attr('string')
+  logo: DS.attr('string')
 });
