2442bf42f99ab4761318a6243323cd77daa6d30a
poglesby94 <paul.oglesby94@icloud.com>
2016-11-22 22:14:58 -0500

    Admin section now only accessible to authenticated users

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+     beforeModel() {
+          if (this.get('session.isAuthenticated')) {
+               this.transitionTo('admin');
+          }else{
+               this.transitionTo('sign-in');
+          }
+     }
 });
